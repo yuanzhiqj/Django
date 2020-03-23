2caf7492f01a8770c20a216c495f4328971f40e9
1721375491@qq.com <1721375491@qq.com>
2020-03-23 21:26:03 +0800

    创建了用户输入，增加了编辑条目的功能

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -10,6 +10,11 @@ urlpatterns = [
     url(r'^new_topic/$',views.new_topic,name='new_topic'),
     #特定主题的详细页面
     url(r'^topics/(?P<topic_id>\d+)/$',views.topic,name='topic'),
+    #用于添加新条目的页面
+    url(r'^new_entry/(?P<topic_id>\d+)/$',views.new_entry,name='new_entry'),
+    #用于编辑条目的页面
+    url(r'^edit_entry/(?P<entry_id>\d+)/$',views.edit_entry,
+        name='edit_entry'),
 ]
 
 app_name='learning_logs'
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from .models import Topic
-from .forms import TopicForm
+from .models import Topic, Entry
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 def index(request):
@@ -33,3 +33,38 @@ def topic(request,topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic,'entries':entries}
     return render(request,'learning_logs/topic.html',context)
+
+def new_entry(request,topic_id):
+    """咋特定的主题中添加条目"""
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        #未提交数据，创建一个空表单
+        form = EntryForm()
+    else:
+        #POST提交的数据，对数据进行处理
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                                        args=[topic_id]))
+    context={'topic':topic,'form':form}
+    return render(request,'learning_logs/new_entry.html',context)
+
+def edit_entry(request,entry_id):
+    """编辑既有条目"""
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+    if request.method != 'POST':
+        #初次请求，使用当前条目填充表单
+        form = EntryForm(instance=entry)
+    else:
+        #POST提交的数据，对数据进行处理
+        form = EntryForm(instance=entry,data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic',
+                                        args=[topic.id]))
+    context = {'entry':entry,'topic':topic,'form':form}
+    return render(request,'learning_logs/edit_entry.html',context)
